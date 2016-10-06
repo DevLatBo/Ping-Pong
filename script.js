c9cd9a1cd4f23b8aeeafa7ae0c8fc77e045d4cd8
05K4R1N <oskr.cbba2010@gmail.com>
2016-10-06 16:22:19 -0400

    eventos por teclado

--- script.js
+++ script.js
@@ -4,32 +4,25 @@ window.onload=function(){
 		ctx=canvas.getContext("2d"),
 		W=window.innerWidth,
 		H=window.innerHeight;
-	//var pelotita=new Pelota();
-	document.addEventListener('keydown',function(evt){
-		console.log(evt.which);
-	});
-	ctx.beginPath();
-	ctx.fillStyle = "red";
-	ctx.strokeStyle="black";
-	ctx.arc(50, 50, 5, 0, 2 * Math.PI, false);
-	ctx.fill();
-	ctx.stroke();
-	ctx.closePath();
-	setInterval(draw,1);
 	
+	var pelotita=new Pelota();
+
+	var jugadores[]=new Jugador();
+
+	setInterval(dibujar,1);
 	function Pelota(){
 		this.x=W/2;
 		this.y=H/2;
 		this.size=10;
 		this.style="#FFFFFF";
-		this.draw=function(){
+		this.dibujar=function(){
 			ctx.beginPath();
 			ctx.fillStyle=this.style;
-			ctx.arc(this.x,this.y,this.size,10,Math.pi*2);
+			ctx.arc(this.x,this.y,this.size,10,0,Math.PI*2);
 			ctx.fill();
 		}
 	}
-	function draw(){
+	function dibujar(){
 		ctx.globalCompositeOperation = "source-over";
 		canvas.width=W;
 		canvas.height=H;
@@ -39,6 +32,44 @@ window.onload=function(){
 		ctx.fillRect(0,360,40,170);
 		ctx.fillStyle="#FF0000";
 		ctx.fillRect(W-40,360,40,150);
+		pelotita.dibujar();
 	}
-
+	function teclaPresionada(evento){
+		console.log(evento.keyCode);
+		var codigo=evento.keyCode;
+		switch(codigo){
+			case 87:
+				console.log("arriba jugador 2");
+				break;
+			case 83:
+				console.log("abajo jugador 2");
+				break;
+			case 38:
+				console.log("arriba jugador 1");
+				break;
+			case 40:
+				console.log("abajo jugador 1");
+				break;
+		}
+	}
+	function teclaLevantada(evento){
+		console.log(evento.keyCode);
+		var codigo=evento.keyCode;
+		switch(codigo){
+			case 87:
+				console.log("arriba jugador 2 [DETENIDO]");
+				break;
+			case 83:
+				console.log("abajo jugador 2 [DETENIDO]");
+				break;
+			case 38:
+				console.log("arriba jugador 1 [DETENIDO]");
+				break;
+			case 40:
+				console.log("abajo jugador 1 [DETENIDO]");
+				break;
+		}
+	}
+	document.addEventListener("keydown",teclaPresionada);
+	document.addEventListener("keyup",teclaLevantada);
 }
