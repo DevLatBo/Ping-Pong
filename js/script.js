c312f1a4c8ac38e4cd673a181d4c29987fc4555d
05K4R1N <oskr.cbba2010@gmail.com>
2018-09-14 14:16:57 -0400

    [M] Adding validation for reboting at the side of the rectangule

--- js/script.js
+++ js/script.js
@@ -16,17 +16,12 @@ window.onload=function(){
 	var pelotita=new Pelota();
 
 	setInterval(dibujar,1);
-	
-	/*
-		Metodo jugador que es representado por cada una de las paletitas.
-		Movimiento de las paletas son de manera vertical cuyo control 
-		es que no pueda pasarse el limite de pantalla del navegador.
-	*/
+
 	function Jugador(lado){
 		this.lado = lado;
-		this.ancho=60;
+		this.ancho=60.5;
 		this.largo=150;
-		this.x=(lado=="izquierda")?0:W-60;
+		this.x=(lado=="izquierda")?0:W-60.5;
 		this.y=generarNumero(0,H-this.largo);
 		this.color="#FF0000";
 		this.recorrido=9;
@@ -52,18 +47,14 @@ window.onload=function(){
 			}
 		}
 	}
-	/*
-		La pelota que es un circulo color blanco tiene una direccion vertical u horizontal al mismo tiempo (direccion diagonal).
-		Cada rebote de la pelota a parte de sus paletas tambien es controlador por el limite superior e inferior de la 
-		pantalla del navegador.
-	*/
+
 	function Pelota(){
 		this.size=15;
 		this.x=W/2;
 		this.y=generarNumero(0,H-this.size);
 		this.style="rgba(255,255,255,0.9)";
-		this.dir_x=generarNumero(0,1)==0?3:-3;
-		this.dir_y=generarNumero(0,1)==0?3:-3;
+		this.dir_x=generarNumero(0,1)==0?2:-2;
+		this.dir_y=generarNumero(0,1)==0?2:-2;
 		this.dibujar=function(){
 			ctx.beginPath();
 			ctx.fillStyle=this.style;
@@ -74,7 +65,7 @@ window.onload=function(){
 			this.x=this.x+this.dir_x;
 			this.y=this.y+this.dir_y;
 			if(this.y<0){
-				this.dir_y=this.dir_y*(-1);
+				this.dir_y *= (-1);
 				reproducirSonido();
 			}
 			if(this.y>H-this.size){
@@ -83,12 +74,22 @@ window.onload=function(){
 			}
 			var jugador1=jugadores[0];
 			var jugador2=jugadores[1];
-			if((this.x>jugador1.x && this.x<jugador1.x+jugador1.ancho) && (this.y>jugador1.y && this.y<jugador1.y+jugador1.largo)){
-				this.dir_x=this.dir_x*(-1);
+			if((this.x>=jugador1.x && this.x<=jugador1.x+jugador1.ancho) &&
+				(this.y>=jugador1.y && this.y<=jugador1.y+jugador1.largo)){
+                if(this.x > jugador1.x && this.x < (jugador1.x+jugador1.ancho)){
+                	this.dir_y *= (-1);
+				}else{
+                    this.dir_x *= (-1);
+                }
 				reproducirSonido();
 			}
-			if((this.x>jugador2.x && this.x<jugador2.x+jugador2.ancho) && (this.y>jugador2.y && this.y<jugador2.y+jugador2.largo)){
-				this.dir_x=this.dir_x*(-1);
+			if((this.x>=jugador2.x && this.x<=jugador2.x+jugador2.ancho) &&
+				(this.y>=jugador2.y && this.y<=jugador2.y+jugador2.largo)){
+                if(this.x > jugador2.x && this.x < (jugador2.x+jugador2.ancho)){
+                    this.dir_y *= (-1);
+                }else{
+                    this.dir_x *= (-1);
+                }
 				reproducirSonido();
 			}
 			if(this.x<0){
@@ -103,10 +104,7 @@ window.onload=function(){
 			}
 		}
 	}
-	/*
-		Simple metodo que consiste en dibujar pelota y movimiento de la misma, como tambien pintar posicion inicial
-		y movimiento de paletas.
-	*/
+
 	function dibujar(){
 		ctx.globalCompositeOperation = "source-over";
 		ctx.fillStyle="#000000";
@@ -128,57 +126,54 @@ window.onload=function(){
 		Control de paletas por medio del teclado por ASCII (tecla presionada y levantada)
 	*/
 	function teclaPresionada(evento){
-		//console.log(evento.keyCode);
 		var codigo=evento.which;
 		switch(codigo){
 			case 79:
-				//console.log("arriba jugador 2");
+				// Arriba jugador 2;
 				movimientos.p2.arriba=true;
 				break;
 			case 75:
-				//console.log("abajo jugador 2");
+				// Abajo jugador 2;
 				movimientos.p2.abajo=true;
 				break;
 			case 87:
-				//console.log("arriba jugador 1");
+				// Arriba jugador 1;
 				movimientos.p1.arriba=true;
 				break;
 			case 83:
-				//console.log("abajo jugador 1");
+				// Abajo jugador 1;
 				movimientos.p1.abajo=true;
 				break;
 		}
 	}
 	function teclaLevantada(evento){
-		//console.log(evento.keyCode);
 		var codigo=evento.which;
 		switch(codigo){
 			case 79:
-				//console.log("arriba jugador 2 [DETENIDO]");
+				// Arriba jugador 2 [DETENIDO];
 				movimientos.p2.arriba=false;
 				break;
 			case 75:
-				//console.log("abajo jugador 2 [DETENIDO]");
+				// Abajo jugador 2 [DETENIDO];
 				movimientos.p2.abajo=false;
 				break;
 			case 87:
-				//console.log("arriba jugador 1 [DETENIDO]");
+				// Arriba jugador 1 [DETENIDO];
 				movimientos.p1.arriba=false;
 				break;
 			case 83:
-				//console.log("abajo jugador 1 [DETENIDO]");
+				// Abajo jugador 1 [DETENIDO]";
 				movimientos.p1.abajo=false;
 				break;
 		}
 	}
-	/*
-		El valor de este metodo devolvera  un valor cuyo resultado afectara en la direccion de la pelota
-	*/
+
 	function generarNumero(min,max){
   		return Math.round(Math.random() * (max - min)) + min;
 	}
+
 	function reproducirSonido(){
-		//document.getElementById('pelota').play();
+
 		var audio = document.getElementById('pelota');
 		if(audio.paused){
 			audio.play();
