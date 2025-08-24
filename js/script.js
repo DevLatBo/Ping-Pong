4a7c27e2b8c75fa76836e702f72cea285e0ecf36
DevLatBo <oskr.cbba2010@gmail.com>
2025-08-24 17:00:28 -0400

    [M] Reorganizing variables, name conventions.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 window.onload=function(){
 	var x=0,y=0;
-	var canvas=document.getElementById("canvas"),
-		ctx=canvas.getContext("2d"),
-		W=window.innerWidth,
-		H=window.innerHeight;
-	canvas.width=W;
-	canvas.height=H;
-	var jugadores=[new Jugador("izquierda"), 
-					new Jugador("derecha")];
-	var marcador=[0,0];
-	var movimientos={
-		p1:{arriba:false,abajo:false},
-		p2:{arriba:false,abajo:false}
-	};
-	var pelotita=new Pelota();
+	var canvas = document.getElementById("canvas"),
+		ctx = canvas.getContext("2d"),
+		W = window.innerWidth,
+		H = window.innerHeight;
+	canvas.width = W;
+	canvas.height = H;
 
-	setInterval(dibujar,1);
-	function Jugador(lado){
+	var players = [
+		new Player("izquierda"), 
+		new Player("derecha")
+	];
+	var scoreboard = [0,0];
+	var paddleDirection = {
+		p1: {
+			up:false,
+			down:false
+		},
+		p2: {
+			up:false,
+			down:false
+		}
+	};
+	var ball = new Ball();
+	setInterval(paintBoard, 1);
+
+	function Player(lado){
 		var jugador = this;
 		jugador.lado = lado;
 		jugador.ancho=60;
@@ -31,24 +40,24 @@ window.onload=function(){
 		}
 		jugador.mover=function(){
 			if(jugador.lado=="izquierda"){
-				if(movimientos.p1.abajo){
+				if(paddleDirection.p1.down){
 					jugador.y=(jugador.y<H-jugador.largo)?jugador.y+jugador.recorrido:jugador.y;
 				}
-				if(movimientos.p1.arriba){
+				if(paddleDirection.p1.up){
 					jugador.y=(jugador.y>0)?jugador.y-jugador.recorrido:jugador.y;
 				}
 			}else{
-				if(movimientos.p2.abajo){
+				if(paddleDirection.p2.down){
 					jugador.y=(jugador.y<H-jugador.largo)?jugador.y+jugador.recorrido:jugador.y;
 				}
-				if(movimientos.p2.arriba){
+				if(paddleDirection.p2.up){
 					jugador.y=(jugador.y>0)?jugador.y-jugador.recorrido:jugador.y;
 				}
 			}
 		}
 	}
 
-	function Pelota(){
+	function Ball(){
 		var pelota = this;
 		pelota.size=15;
 		pelota.x=W/2;
@@ -71,48 +80,48 @@ window.onload=function(){
 			if(pelota.y>H-pelota.size){
 				pelota.dir_y=pelota.dir_y*(-1);
 			}
-			var jugador1=jugadores[0];
-			var jugador2=jugadores[1];
+			var jugador1 = players[0];
+			var jugador2 = players[1];
 			
 			if((pelota.x - pelota.size < (jugador1.x+jugador1.ancho)) && (pelota.y >= jugador1.y && pelota.y <= (jugador1.largo + jugador1.y))){
 				// Izquierda
 				pelota.dir_x *= (-1);
-				reproducirSonido();
+				hitBall();
 			}
 			if((pelota.x + pelota.size >= jugador2.x) && (pelota.y >= jugador2.y && pelota.y <= (jugador2.largo + jugador2.y))){
 				// Derecha
 				pelota.dir_x *= (-1);
-				reproducirSonido();
+				hitBall();
 			}
 			if(pelota.x<0){
-				marcador[1]++;
+				scoreboard[1]++;
 				pelota.x=W/2;
 				pelota.y=generarNumero(0,H-pelota.size);
 			}
 			if(pelota.x>W-pelota.size){
-				marcador[0]++;
+				scoreboard[0]++;
 				pelota.x=W/2;
 				pelota.y=generarNumero(0,H-pelota.size);
 			}
 		}
 	}
 
-	function dibujar(){
+	function paintBoard(){
 		ctx.globalCompositeOperation = "source-over";
 		ctx.fillStyle="#000000";
 		ctx.fillRect(0,0,W,H);
 
 		ctx.font="120px Helvetica";
 		ctx.fillStyle="#FFFFFF";
-		ctx.fillText(""+marcador[0],300,100);
-		ctx.fillText(""+marcador[1],1100,100);
+		ctx.fillText("" + scoreboard[0], 300, 100);
+		ctx.fillText("" + scoreboard[1], 1100, 100);
 
-		for(var i=0;i<jugadores.length;i++){
-			jugadores[i].dibujar();
-			jugadores[i].mover();
+		for(var i=0;i<players.length;i++){
+			players[i].dibujar();
+			players[i].mover();
 		}
-		pelotita.dibujar();
-		pelotita.mover();
+		ball.dibujar();
+		ball.mover();
 	}
 
 	//Control de paletas por medio del teclado por ASCII (tecla presionada y levantada)
@@ -121,19 +130,19 @@ window.onload=function(){
 		switch(codigo){
 			case 79:
 				// Arriba jugador 2;
-				movimientos.p2.arriba=true;
+				paddleDirection.p2.up = true;
 				break;
 			case 75:
 				// Abajo jugador 2;
-				movimientos.p2.abajo=true;
+				paddleDirection.p2.down = true;
 				break;
 			case 87:
 				// Arriba jugador 1;
-				movimientos.p1.arriba=true;
+				paddleDirection.p1.up = true;
 				break;
 			case 83:
 				// Abajo jugador 1;
-				movimientos.p1.abajo=true;
+				paddleDirection.p1.down = true;
 				break;
 		}
 	}
@@ -142,19 +151,19 @@ window.onload=function(){
 		switch(codigo){
 			case 79:
 				// Arriba jugador 2 [DETENIDO];
-				movimientos.p2.arriba=false;
+				paddleDirection.p2.up = false;
 				break;
 			case 75:
 				// Abajo jugador 2 [DETENIDO];
-				movimientos.p2.abajo=false;
+				paddleDirection.p2.down = false;
 				break;
 			case 87:
 				// Arriba jugador 1 [DETENIDO];
-				movimientos.p1.arriba=false;
+				paddleDirection.p1.up = false;
 				break;
 			case 83:
 				// Abajo jugador 1 [DETENIDO]";
-				movimientos.p1.abajo=false;
+				paddleDirection.p1.down = false;
 				break;
 		}
 	}
@@ -163,15 +172,16 @@ window.onload=function(){
   		return Math.round(Math.random() * (max - min)) + min;
 	}
 
-	function reproducirSonido(){
-		var audio = document.getElementById('pelota');
-		if(audio.paused){
+	function hitBall(){
+		var audio = document.getElementById('hitBall');
+		if (audio.paused){
 			audio.play();
-		}else{
+		} else {
 			audio.pause();
 			audio.currentTime = 0;
 		}
 	}
+	
 	document.addEventListener("keydown",teclaPresionada);
 	document.addEventListener("keyup",teclaLevantada);
 }
